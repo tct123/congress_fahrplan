055b170ef0dc9c75583a59d71e4e4938d5682465
tct123 <42028373+tct123@users.noreply.github.com>
2025-05-05 22:59:53 +0200

    .

--- android/app/src/main/java/de.delusionsoftware.congress_fahrplan/MainActivity.java
+++
@@ -1,6 +0,0 @@
-package de.delusionsoftware.congress_fahrplan;
-
-import io.flutter.embedding.android.FlutterActivity;
-
-public class MainActivity extends FlutterActivity {
-}
---
+++ android/app/src/main/kotlin/com/example/congress_fahrplan/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.congress_fahrplan
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
